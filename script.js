2500dab679250aba0876f3cf93a74932e0a6a255
Mugakai <74754507+Mugakai@users.noreply.github.com>
2020-11-27 00:51:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const { Client } = require('discord.js');
 const bot = new Client();
 
+const settings = { prefix: "+" };
+const chars = [
+    '🤩🤡🤩',
+    '🤡🤩🤡',
+    '🤡🤡🤡'
+];
+
 bot.on('ready', () => {
     console.log(`Active.`);
+});
+
+bot.on('message', (msg) => {
+    const args = msg.content.slice(settings.prefix.length).trim().split(' ');
+    if (chars.includes(args.values)) {
+        return msg.reply('.>>.<<.');
+    }     
 });
 
 bot.login(process.env.AUTH_TOKEN);
